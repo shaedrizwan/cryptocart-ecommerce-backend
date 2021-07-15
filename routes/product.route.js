5d35516cde42549a86a747cdd55beeab8fae0c79
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-07-15 13:44:29 +0530

    Feat: AddProduct route added

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const {verifyToken} = require('../middlewares/auth.middleware')
+const Product = require('../models/product.model')
 
-router.use('/',verifyToken)
 router.route('/')
-    .get((req,res)=>{
-        res.send('Product route successful with token')
+    .get(async (req,res)=>{
+        try{
+            const products = await Product.find({})
+            res.json({success:true,products})
+        }catch(err){
+            res.json({success:false,error:err.message})
+        }
+    })
+
+
+router.use('/addProduct',verifyToken)
+router.route('/addProduct')
+    .post(async (req,res)=>{
+        const newProduct = req.body
+        try{
+            const addNewProduct = new Product(newProduct)
+            const addedProduct = await addNewProduct.save()
+            res.json({success:true,message:"Product successfully added", addedProduct})
+        }catch(err){
+            res.json({success:false,message:"Failed to add the product",error:err.message})
+        }
     })
 
 module.exports = router

829287e13e588d64e337df4a879204c57af5644f
shaedrizwan <50213576+shaedrizwan@users.noreply.github.com>
2021-07-27 23:32:01 +0530

    Fix: Password hashing on each User save

--- models/user.model.js
+++ models/user.model.js
@@ -35,16 +35,5 @@ const userSchema = new Schema({
     cart:[cartSchema]
 })
 
-userSchema.pre('save',async function(next){
-    try{
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(this.password,salt)
-        this.password = hashedPassword
-        next()
-    }catch(err){
-        next(err)
-    }
-})
-
 
 module.exports = mongoose.model("User",userSchema)
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,18 +3,21 @@ const { checkLogin } = require('../middlewares/auth.middleware')
 const router = express.Router()
 const User = require('../models/user.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 router.route('/signup')
     .post(async(req,res)=>{
         try{
             const user = req.body
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(user.password,salt)
+            user.password = hashedPassword
             const newUser = new User(user)
             const addUser = await newUser.save()
             res.json({success:true,addUser})
         }catch(err){
             res.json({success:false,error:err.message})
         }
-    
     })
 
 router.use(checkLogin)
